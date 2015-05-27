8b270663478cfaeb1bb3343a40ed6750c63aae28
Scott Williams <williamsjs1224@gmail.com>
2015-05-27 14:06:19 -0400

    add test

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class VotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "votes have index action" do
+    get :index
+    assert_response :success
+  end
 end
